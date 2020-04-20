305ec09ff90434a30c3c6adfd01fc0f444bd1650
dadaiwei <dadaiwei@corp.netease.com>
2020-04-20 12:39:20 +0800

    :art:Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Main from './router/index';
-import './style/index.scss';
+import './style/index.less';
 
 function App() {
   return <div>
